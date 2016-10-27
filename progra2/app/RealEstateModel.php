9a1313846b3d2cb58231fc1f68f4eb44ccb9fb8a
crissalas94 <cs.salas94@gmail.com>
2016-10-27 16:40:21 -0600

    login_window

--- progra2/app/RealEstateModel.php
+++ progra2/app/RealEstateModel.php
@@ -6,6 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class RealEstateModel extends Model
 {
-    protected $fillable = array('agregar aqui todos los atributos de bienesraices');
-    protected $hidden = array('created_at', 'updated_at');
+    protected $table = 'bienesraices';
 }
--- progra2FrontEnd/login_register.module/login_register.controller.js
+++ progra2FrontEnd/login_register.module/login_register.controller.js
@@ -4,6 +4,21 @@ angular.module('app')
     .controller('loginRegisterController', ['$state', function ($stateProvider) {
         var th = this;
 
-        th.enviar = function () {
+        th.register = {
+            name: '',
+            lastname: '',
+            email: '',
+            password: ''
         };
+
+        th.login = {
+            user: '',
+            password: ''
+        };
+
+        th.x = function () {
+            console.log(th.register);
+
+        };
+
     }]);
